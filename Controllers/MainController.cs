0f724e3a76f1c9cf05cea8d412d3611c9ffd5f45
Daniel Lundeby <daniel.m.lundeby@gmail.com>
2023-10-29 11:35:22 +0100

    Add weather endpoint

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -7,9 +7,23 @@ namespace Toolbox.Controllers;
 [Route("")]
 public class MainController : Controller
 {
-    [HttpGet("[action]")]
+    [HttpGet("hello")]
     public IActionResult Hello()
     {
         return Ok("Hello World!");
+    }
+
+    [HttpGet("weather")]
+    public IEnumerable<WeatherForecast> Weather()
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast(
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ));
     }
 }
